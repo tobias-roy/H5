8b01d6880d541a436536693e89b2206d3f319fbb
tobias-roy <tobiasroy94@gmail.com>
2025-03-17 12:37:40 +0100

    Created a barebone ML model that tries to identify positive and negative sentences based on 1000 yelp reviews.

---
+++ ML/Models.cs
@@ -0,0 +1,19 @@
+using Microsoft.ML.Data;
+
+namespace ML;
+
+public class Models
+{
+    public class SentimentData
+    {
+        [LoadColumn(0)] public string? Text { get; set; }
+        [LoadColumn(1), ColumnName("Label")] public bool Sentiment { get; set; }
+    }
+
+    public class SentimentPrediction : SentimentData
+    {
+        [ColumnName("PredictedLabel")] public bool Prediction { get; set; }
+        public float Probability { get; set; }
+        public float Score { get; set; }
+    }
+}
---
+++ ML/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ML;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Spectre.Console;
+using Console = Spectre.Console.AnsiConsole;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var ctx = new MLContext();
+        try
+        {
+            ctx.Model.Load("model.zip", out var schema);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteException(ex);
+        }
+        
+        var dataView = ctx.Data.LoadFromTextFile<Models.SentimentData>("yelp.txt", hasHeader: false, separatorChar: '\t');
+        var splitDataView = ctx.Data.TrainTestSplit(dataView, testFraction: 0.33);
+        var estimator = ctx.Transforms.Text.FeaturizeText(
+                outputColumnName: "Features",
+                inputColumnName: nameof(Models.SentimentData.Text)
+            )
+            .Append(ctx.BinaryClassification.Trainers.SdcaLogisticRegression(featureColumnName: "Features"));
+        
+        ITransformer model = default!;
+        var rule = new Rule("Create and Train Model");
+        Console.Live(rule).Start(console =>
+        {
+            //Model is training
+            model = estimator.Fit(splitDataView.TrainSet);
+            var predictions = model.Transform(splitDataView.TestSet);
+
+            rule.Title = "Training Complete, Evaluating Accuracy.";
+            console.Refresh();
+            
+            //Evaluate the accuracy
+            var metrics = ctx.BinaryClassification.Evaluate(predictions);
+            
+            //Create table for the console
+            var table = new Table()
+                .MinimalBorder()
+                .Title("Model Accuracy");
+            table.AddColumns("Accuracy", "AreaUnderRoc", "F1Score");
+            table.AddRow($"{metrics.Accuracy:P2}", $"{metrics.AreaUnderRocCurve:P2}", $"{metrics.F1Score:P2}");
+            
+            console.UpdateTarget(table);
+            console.Refresh();
+        });
+        
+        //Save the model we just trained
+        ctx.Model.Save(model, dataView.Schema, "model.zip");
+
+        //Loop over asking for user input
+        while (true)
+        {
+            var text = AnsiConsole.Ask<string>("What's your [green]review text[/]?");
+            var engine = ctx.Model.CreatePredictionEngine<Models.SentimentData, Models.SentimentPrediction>(model);
+
+            var input = new Models.SentimentData { Text = text };
+            var result = engine.Predict(input);
+            var style = result.Prediction
+                ? (color: "green", emoji: "👍")
+                : (color: "red", emoji: "👎");
+            Console.MarkupLine($"{style.emoji} [{style.color}]\"{text}\" ({result.Probability:P00})[/] ");
+        }
+    }
+}
